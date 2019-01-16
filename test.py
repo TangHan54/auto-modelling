4e5b60be003822ce6fc176525b1e2cdffd7f6ab8
Tang Han <han@healint.com>
2019-01-16 14:59:27 +0800

    updated the package

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from auto_modelling.classifion import GoClassify
-from sklearn.metrics import accuracy_score
+from auto_modelling.regression import GoRegress
+from sklearn.metrics import accuracy_score, mean_squared_error
 
 data = pd.read_csv('data/test.csv')
 
@@ -13,7 +14,12 @@ x_train, x_test, y_train, y_test = train_test_split(
     y,
     test_size = 0.3)
 
-clf = GoClassify()
-best = clf.train(x_train, y_train)
-y_pred = best.predict(x_test)
-print(f'accuracy: {accuracy_score(y_pred,y_test)}')
+# clf = GoClassify()
+# best = clf.train(x_train, y_train)
+# y_pred = best.predict(x_test)
+# print(f'accuracy: {accuracy_score(y_pred,y_test)}')
+
+reg = GoRegress()        
+best_reg = reg.train(x_train,y_train)
+y_pred = best_reg.predict(x_test)
+print(f'mse: {mean_squared_error(y_pred,y_test)}')
