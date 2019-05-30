a292f8f486f0ee56987b5f3ac904aa00fd712609
han <han@healint.com>
2019-05-30 14:16:44 +0800

    updated test file

--- test.py
+++ test.py
@@ -2,10 +2,11 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from auto_modelling.classifion import GoClassify
 from auto_modelling.regression import GoRegress
+from auto_modelling.preprocess import DataManager
 from sklearn.metrics import accuracy_score, mean_squared_error
 import logging
 
-data = pd.read_csv('data/test.csv')
+data = pd.read_csv('data/clean_test_data.csv')
 
 y = data['trigger_flag']
 X = data.drop('trigger_flag', axis = 1)
@@ -13,7 +14,11 @@ X = data.drop('trigger_flag', axis = 1)
 x_train, x_test, y_train, y_test = train_test_split(
     X, 
     y,
-    test_size = 0.3)
+    test_size = 0.1)
+
+dm = DataManager()
+train, test = dm.drop_sparse_columns(x_train, x_test)
+train, test = dm.process_data(x_train, x_test)
 
 clf = GoClassify()
 best = clf.train(x_train, y_train)
